7deb0e1304d5c57b6d49678c00c26b313822f47d
Helsinki1 <davidxionghl@gmail.com>
2025-01-11 00:02:06 -0500

    added routes + nav bar

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className="loginPage">
+      <p> Sign In with Google to Continue</p>
+      <button className="login-with-google-btn">
+        Sign in with Google
+      </button>
+    </div>
+  );
+}
+
+export default Login
